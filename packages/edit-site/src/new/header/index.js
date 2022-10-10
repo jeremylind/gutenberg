a56a45723f5abbf4406e687de01325341032e74b
Riad Benguella <benguella@gmail.com>
2022-10-10 09:08:37 +0100

    Simplify header

--- packages/edit-site/src/new/header/index.js
+++ packages/edit-site/src/new/header/index.js
@@ -1,15 +1,22 @@
 /**
  * WordPress dependencies
  */
-import { useSelect } from '@wordpress/data';
-import { __unstableAnimatePresence as AnimatePresence } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import {
+	__unstableAnimatePresence as AnimatePresence,
+	__experimentalHStack as HStack,
+	__unstableMotion as motion,
+	Button,
+	FlexBlock,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import { store as editSiteStore } from '../../store';
-import HeaderEditMode from '../header-edit-mode';
-import HeaderViewMode from '../header-view-mode';
+import EditorToolbars from '../../components/header';
+import SiteIconAndTitle from '../site-icon-and-title';
 
 export default function Header() {
 	const { canvasMode } = useSelect(
@@ -18,14 +25,40 @@ export default function Header() {
 		} ),
 		[]
 	);
+	const { __unstableSetCanvasMode } = useDispatch( editSiteStore );
 
 	return (
-		<AnimatePresence exitBeforeEnter={ true }>
-			{ canvasMode === 'view' ? (
-				<HeaderViewMode key="view" />
-			) : (
-				<HeaderEditMode key="edit" />
-			) }
-		</AnimatePresence>
+		<HStack>
+			<Button
+				className="edit-site-new-header__toggle"
+				label={ __( 'Toggle Navigation Sidebar' ) }
+				onClick={ () =>
+					__unstableSetCanvasMode(
+						canvasMode === 'view' ? 'edit' : 'view'
+					)
+				}
+			>
+				{ canvasMode === 'edit' && (
+					<SiteIconAndTitle showTitle={ false } />
+				) }
+				{ canvasMode === 'view' && __( 'Edit' ) }
+			</Button>
+			<FlexBlock>
+				<AnimatePresence exitBeforeEnter>
+					{ canvasMode === 'edit' && (
+						<motion.div
+							initial={ { opacity: 0 } }
+							animate={ { opacity: 1 } }
+							exit={ {
+								opacity: 0,
+							} }
+							transition={ { type: 'tween', duration: 0.5 } }
+						>
+							<EditorToolbars />
+						</motion.div>
+					) }
+				</AnimatePresence>
+			</FlexBlock>
+		</HStack>
 	);
 }
