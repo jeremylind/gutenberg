66307ad22dc076101d70803b637332369cf93ad9
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-10-12 11:03:52 -0500

    fix: Inner Blocks supports Voice Control
    A block's accessible state should take into consideration all descendent
    blocks, rather than merely its child blocks. This allows iOS Voice
    Control to take action upon text inputs within currently selected,
    nested blocks.

--- packages/block-editor/src/components/block-list/block.native.js
+++ packages/block-editor/src/components/block-list/block.native.js
@@ -188,7 +188,7 @@ class BlockListBlock extends Component {
 			getStylesFromColorScheme,
 			marginVertical,
 			marginHorizontal,
-			isInnerBlockSelected,
+			isDescendentBlockSelected,
 			name,
 			draggingEnabled,
 			draggingClientId,
@@ -205,7 +205,7 @@ class BlockListBlock extends Component {
 			order + 1
 		);
 		const { isFullWidth, isContainerRelated } = alignmentHelpers;
-		const accessible = ! ( isSelected || isInnerBlockSelected );
+		const accessible = ! ( isSelected || isDescendentBlockSelected );
 		const screenWidth = Math.floor( Dimensions.get( 'window' ).width );
 		const isScreenWidthEqual = blockWidth === screenWidth;
 		const isScreenWidthWider = blockWidth < screenWidth;
@@ -335,7 +335,11 @@ export default compose( [
 
 		const order = getBlockIndex( clientId );
 		const isSelected = isBlockSelected( clientId );
-		const isInnerBlockSelected = hasSelectedInnerBlock( clientId );
+		const checkDeep = true;
+		const isDescendentBlockSelected = hasSelectedInnerBlock(
+			clientId,
+			checkDeep
+		);
 		const block = getBlock( clientId );
 		const { name, attributes, isValid } = block || {};
 
@@ -395,7 +399,7 @@ export default compose( [
 			draggingClientId,
 			draggingEnabled,
 			isSelected,
-			isInnerBlockSelected,
+			isDescendentBlockSelected,
 			isValid,
 			isParentSelected,
 			firstToSelectId,
